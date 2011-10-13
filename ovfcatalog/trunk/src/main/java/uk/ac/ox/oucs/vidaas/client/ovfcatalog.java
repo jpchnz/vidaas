293464a344e13caef8bbfbc674dc76bc2a9e3d04
christian.fernau <christian.fernau@gmail.com>
2011-10-13 14:41:47 +0000

    removed i18n

--- ovfcatalog/trunk/src/main/java/uk/ac/ox/oucs/vidaas/client/ovfcatalog.java
+++ ovfcatalog/trunk/src/main/java/uk/ac/ox/oucs/vidaas/client/ovfcatalog.java
@@ -34,15 +34,13 @@ public class ovfcatalog implements EntryPoint {
    */
   private final GreetingServiceAsync greetingService = GWT.create(GreetingService.class);
 
-  private final Messages messages = GWT.create(Messages.class);
-
   /**
    * This is the entry point method.
    */
   public void onModuleLoad() {
-    final Button sendButton = new Button( messages.sendButton() );
+    final Button sendButton = new Button( "Send" );
     final TextBox nameField = new TextBox();
-    nameField.setText( messages.nameField() );
+    nameField.setText( "message text" );
     final Label errorLabel = new Label();
 
     // We can add style names to widgets
