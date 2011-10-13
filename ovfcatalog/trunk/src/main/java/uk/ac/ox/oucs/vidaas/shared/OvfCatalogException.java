cd0df4df202b7ae2a609008ced9853b899d75bc7
christian.fernau <christian.fernau@gmail.com>
2011-10-13 19:00:27 +0000

    new exception type

---
+++ ovfcatalog/trunk/src/main/java/uk/ac/ox/oucs/vidaas/shared/OvfCatalogException.java
@@ -0,0 +1,24 @@
+package uk.ac.ox.oucs.vidaas.shared;
+
+/**
+ * Simple Exception class
+ * 
+ * @author chris
+ *
+ */
+public class OvfCatalogException extends Exception {
+
+	/**
+	 * serial
+	 */
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * Constructor
+	 * 
+	 * @param message message
+	 */
+	public OvfCatalogException(String message) {
+		super(message);
+	}
+}
