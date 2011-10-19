3fc1c346faf01f0e8fc9a28359a113ca3ad9b168
christian.fernau@gmail.com <christian.fernau@gmail.com@24beba35-015c-934d-eeb6-620b1e2b2385>
2011-10-19 17:00:32 +0000

    testing vmware SDK

--- ovfcatalog/trunk/src/main/java/uk/ac/ox/oucs/vidaas/client/ovfcatalog.java
+++ ovfcatalog/trunk/src/main/java/uk/ac/ox/oucs/vidaas/client/ovfcatalog.java
@@ -1,147 +1,99 @@
 package uk.ac.ox.oucs.vidaas.client;
 
-import uk.ac.ox.oucs.vidaas.shared.FieldVerifier;
+import uk.ac.ox.oucs.vidaas.shared.VmValue;
+
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
-import com.google.gwt.event.dom.client.KeyCodes;
-import com.google.gwt.event.dom.client.KeyUpEvent;
-import com.google.gwt.event.dom.client.KeyUpHandler;
 import com.google.gwt.user.client.rpc.AsyncCallback;
+import com.google.gwt.user.client.ui.Anchor;
 import com.google.gwt.user.client.ui.Button;
-import com.google.gwt.user.client.ui.DialogBox;
-import com.google.gwt.user.client.ui.HTML;
+import com.google.gwt.user.client.ui.FlexTable;
 import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.RootPanel;
-import com.google.gwt.user.client.ui.TextBox;
-import com.google.gwt.user.client.ui.VerticalPanel;
 
 /**
  * Entry point classes define <code>onModuleLoad()</code>.
  */
 public class ovfcatalog implements EntryPoint {
-  /**
-   * The message displayed to the user when the server cannot be reached or
-   * returns an error.
-   */
-  private static final String SERVER_ERROR = "An error occurred while "
-      + "attempting to contact the server. Please check your network "
-      + "connection and try again.";
 
-  /**
-   * Create a remote service proxy to talk to the server-side Greeting service.
-   */
-  private final GreetingServiceAsync greetingService = GWT.create(GreetingService.class);
+	/**
+	 * Create a remote service proxy to talk to the server-side Greeting
+	 * service.
+	 */
+	private final GreetingServiceAsync greetingService = GWT
+			.create(GreetingService.class);
 
-  /**
-   * This is the entry point method.
-   */
-  public void onModuleLoad() {
-    final Button sendButton = new Button( "Send" );
-    final TextBox nameField = new TextBox();
-    nameField.setText( "message text" );
-    final Label errorLabel = new Label();
+	/**
+	 * This is the entry point method.
+	 */
+	public void onModuleLoad() {
 
-    // We can add style names to widgets
-    sendButton.addStyleName("sendButton");
+		greetingService.getVMs(new AsyncCallback<VmValue[]>() {
 
-    // Add the nameField and sendButton to the RootPanel
-    // Use RootPanel.get() to get the entire body element
-    RootPanel.get("nameFieldContainer").add(nameField);
-    RootPanel.get("sendButtonContainer").add(sendButton);
-    RootPanel.get("errorLabelContainer").add(errorLabel);
+			@Override
+			public void onFailure(Throwable arg0) {
+				arg0.printStackTrace();
+			}
 
-    // Focus the cursor on the name field when the app loads
-    nameField.setFocus(true);
-    nameField.selectAll();
+			@Override
+			public void onSuccess(VmValue[] vms) {
+				updateList("vmlistContainer", vms);
+			}
 
-    // Create the popup dialog box
-    final DialogBox dialogBox = new DialogBox();
-    dialogBox.setText("Remote Procedure Call");
-    dialogBox.setAnimationEnabled(true);
-    final Button closeButton = new Button("Close");
-    // We can set the id of a widget by accessing its Element
-    closeButton.getElement().setId("closeButton");
-    final Label textToServerLabel = new Label();
-    final HTML serverResponseLabel = new HTML();
-    VerticalPanel dialogVPanel = new VerticalPanel();
-    dialogVPanel.addStyleName("dialogVPanel");
-    dialogVPanel.add(new HTML("<b>Sending name to the server:</b>"));
-    dialogVPanel.add(textToServerLabel);
-    dialogVPanel.add(new HTML("<br><b>Server replies:</b>"));
-    dialogVPanel.add(serverResponseLabel);
-    dialogVPanel.setHorizontalAlignment(VerticalPanel.ALIGN_RIGHT);
-    dialogVPanel.add(closeButton);
-    dialogBox.setWidget(dialogVPanel);
+		});
+		
+		greetingService.getTemplates(new AsyncCallback<VmValue[]>() {
 
-    // Add a handler to close the DialogBox
-    closeButton.addClickHandler(new ClickHandler() {
-      public void onClick(ClickEvent event) {
-        dialogBox.hide();
-        sendButton.setEnabled(true);
-        sendButton.setFocus(true);
-      }
-    });
+			@Override
+			public void onFailure(Throwable arg0) {
+				arg0.printStackTrace();
+			}
 
-    // Create a handler for the sendButton and nameField
-    class MyHandler implements ClickHandler, KeyUpHandler {
-      /**
-       * Fired when the user clicks on the sendButton.
-       */
-      public void onClick(ClickEvent event) {
-        sendNameToServer();
-      }
+			@Override
+			public void onSuccess(VmValue[] vms) {
+				updateList("templatelistContainer", vms);
+			}
 
-      /**
-       * Fired when the user types in the nameField.
-       */
-      public void onKeyUp(KeyUpEvent event) {
-        if (event.getNativeKeyCode() == KeyCodes.KEY_ENTER) {
-          sendNameToServer();
-        }
-      }
+		});
+		
+	}
 
-      /**
-       * Send the name from the nameField to the server and wait for a response.
-       */
-      private void sendNameToServer() {
-        // First, we validate the input.
-        errorLabel.setText("");
-        String textToServer = nameField.getText();
-        if (!FieldVerifier.isValidName(textToServer)) {
-          errorLabel.setText("Please enter at least four characters");
-          return;
-        }
+	private void updateList(String list, VmValue[] vms) {
+		FlexTable flexTableVmlist = new FlexTable();
+		RootPanel.get(list).add(flexTableVmlist);
+		int i = 0;
+		for (VmValue vm : vms) {
+			int j = 0;
+			flexTableVmlist.setText(i, j++, vm.getName());
+			flexTableVmlist.setText(i, j++, "" + vm.getStatus());
 
-        // Then, we send the input to the server.
-        sendButton.setEnabled(false);
-        textToServerLabel.setText(textToServer);
-        serverResponseLabel.setText("");
-        greetingService.greetServer(new AsyncCallback<String>() {
-          public void onFailure(Throwable caught) {
-            // Show the RPC error message to the user
-            dialogBox.setText("Remote Procedure Call - Failure");
-            serverResponseLabel.addStyleName("serverResponseLabelError");
-            serverResponseLabel.setHTML(SERVER_ERROR);
-            dialogBox.center();
-            closeButton.setFocus(true);
-          }
+			String[] ips = vm.getIpAddresses();
+			if(ips != null) {
+				for (String ip : ips) {
+					flexTableVmlist.setWidget(i, j++, new Anchor(ip, "http://" + ip + "/"));
+				}
+			}
+			String[] files = vm.getFiles();
+			if(files !=null) {
+				for (String file : files) {
+					flexTableVmlist.setWidget(i, j++, new Label(file));
+				}
+			}
+			if(vm.isCreateable()) {
+				flexTableVmlist.setWidget(i, j++, new Button("buy", new ClickHandler() {
+					
+					@Override
+					public void onClick(ClickEvent arg0) {
+						// TODO Auto-generated method stub
+						
+					}
+				}));
+			}
+			i++;
+		}
+	}
+	
 
-          public void onSuccess(String result) {
-            dialogBox.setText("Remote Procedure Call");
-            serverResponseLabel.removeStyleName("serverResponseLabelError");
-            serverResponseLabel.setHTML(result);
-            dialogBox.center();
-            closeButton.setFocus(true);
-          }
-        });
-      }
-    }
-
-    // Add a handler to send the name to the server
-    MyHandler handler = new MyHandler();
-    sendButton.addClickHandler(handler);
-    nameField.addKeyUpHandler(handler);
-  }
 }
