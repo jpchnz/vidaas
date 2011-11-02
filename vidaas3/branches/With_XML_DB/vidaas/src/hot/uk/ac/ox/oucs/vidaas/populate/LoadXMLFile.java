701f208a113e6325bc515b455384764f298c5c96
thestoat@gmail.com <thestoat@gmail.com@24beba35-015c-934d-eeb6-620b1e2b2385>
2011-11-02 10:09:01 +0000

    Backed out previous changes

--- vidaas3/branches/With_XML_DB/vidaas/src/hot/uk/ac/ox/oucs/vidaas/populate/LoadXMLFile.java
+++ vidaas3/branches/With_XML_DB/vidaas/src/hot/uk/ac/ox/oucs/vidaas/populate/LoadXMLFile.java
@@ -3,8 +3,8 @@ package uk.ac.ox.oucs.vidaas.populate;
 import java.io.*;
 import java.util.*;
 
-//import com.sleepycat.db.*;
-//import com.sleepycat.dbxml.*;
+import com.sleepycat.db.*;
+import com.sleepycat.dbxml.*;
 
 public class LoadXMLFile {
 	
