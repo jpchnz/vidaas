79fdd0244eeee61f2db02062fa7292a27000cc56
thestoat@gmail.com <thestoat@gmail.com@24beba35-015c-934d-eeb6-620b1e2b2385>
2011-11-02 08:01:43 +0000

    Removed unused imports

--- vidaas3/branches/With_XML_DB/vidaas/src/hot/uk/ac/ox/oucs/vidaas/utility/FileUploadBean.java
+++ vidaas3/branches/With_XML_DB/vidaas/src/hot/uk/ac/ox/oucs/vidaas/utility/FileUploadBean.java
@@ -1,110 +1,104 @@
 package uk.ac.ox.oucs.vidaas.utility;
 
-import java.io.IOException;
-import java.io.OutputStream;
+import java.io.File;
 import java.util.ArrayList;
 
-
-import java.io.DataOutputStream;
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-
-import org.richfaces.event.UploadEvent;
-import org.richfaces.model.UploadItem;
 import org.jboss.seam.annotations.Name;
 import org.jboss.seam.contexts.Contexts;
+import org.richfaces.event.UploadEvent;
+import org.richfaces.model.UploadItem;
 
 @Name("fileUploadBean")
-public class FileUploadBean{
-    
-    private ArrayList<File> files = new ArrayList<File>();
-    private int uploadsAvailable = 5;
-    private boolean autoUpload = false;
-    private boolean useFlash = false;
-    public int getSize() {
-        if (getFiles().size()>0){
-            return getFiles().size();
-        }else {
-            return 0;
-        }
-    }
+public class FileUploadBean {
 
-    public FileUploadBean() {
-    }
-    
-    public void listener(UploadEvent event) throws Exception{
-        UploadItem item = event.getUploadItem();
-        if(item != null){
-         File file = item.getFile();
-        System.out.println(item.getFileName());
-        
-        //byte[] itemBytes = item.getData();
-        
-        System.out.println(file.length());
-        System.out.println(file.getAbsolutePath());
-        
-         ((uk.ac.ox.oucs.vidaas.session.CreateController) Contexts.getSessionContext().get(
-		  "createController")).createXMLDatabase(item.getFileName(), file.getAbsolutePath(), file.length() );
-		 
-        
-        /*
-        String FILENAME = "binary.dat";
-        DataOutputStream os = new DataOutputStream(new FileOutputStream(
-            FILENAME));
-        os.write(item.getData());
-        os.close();*/
-        /*Attachment file = new AttachmentHome().getInstance();
-        file.setSize(item.getData().length);
-        file.setName(item.getFileName());
-        file.setData(item.getData());
-        files.add(file);
-        uploadsAvailable--;*/
-        } else {
-         System.out.println("Item is null .... !");
-        }
-    }  
-      
-    public String clearUploadData() {
-        files.clear();
-        setUploadsAvailable(5);
-        return null;
-    }
-    
-    public long getTimeStamp(){
-        return System.currentTimeMillis();
-    }
-    
-    public ArrayList<File> getFiles() {
-        return files;
-    }
+	private ArrayList<File> files = new ArrayList<File>();
+	private int uploadsAvailable = 5;
+	private boolean autoUpload = false;
+	private boolean useFlash = false;
 
-    public void setFiles(ArrayList<File> files) { 
-        this.files = files;
-    }
+	public int getSize() {
+		if (getFiles().size() > 0) {
+			return getFiles().size();
+		} else {
+			return 0;
+		}
+	}
 
-    public int getUploadsAvailable() {
-        return uploadsAvailable;
-    }
+	public FileUploadBean() {
+	}
 
-    public void setUploadsAvailable(int uploadsAvailable) {
-        this.uploadsAvailable = uploadsAvailable;
-    }
+	public void listener(UploadEvent event) throws Exception {
+		UploadItem item = event.getUploadItem();
+		if (item != null) {
+			File file = item.getFile();
+			System.out.println(item.getFileName());
 
-    public boolean isAutoUpload() {
-        return autoUpload;
-    }
+			// byte[] itemBytes = item.getData();
 
-    public void setAutoUpload(boolean autoUpload) {
-        this.autoUpload = autoUpload;
-    }
+			System.out.println(file.length());
+			System.out.println(file.getAbsolutePath());
 
-    public boolean isUseFlash() {
-        return useFlash;
-    }
+			((uk.ac.ox.oucs.vidaas.session.CreateController) Contexts
+					.getSessionContext().get("createController"))
+					.createXMLDatabase(item.getFileName(),
+							file.getAbsolutePath(), file.length());
 
-    public void setUseFlash(boolean useFlash) {
-        this.useFlash = useFlash;
-    }
+			/*
+			 * String FILENAME = "binary.dat"; DataOutputStream os = new
+			 * DataOutputStream(new FileOutputStream( FILENAME));
+			 * os.write(item.getData()); os.close();
+			 */
+			/*
+			 * Attachment file = new AttachmentHome().getInstance();
+			 * file.setSize(item.getData().length);
+			 * file.setName(item.getFileName()); file.setData(item.getData());
+			 * files.add(file); uploadsAvailable--;
+			 */
+		} else {
+			System.out.println("Item is null .... !");
+		}
+	}
+
+	public String clearUploadData() {
+		files.clear();
+		setUploadsAvailable(5);
+		return null;
+	}
+
+	public long getTimeStamp() {
+		return System.currentTimeMillis();
+	}
+
+	public ArrayList<File> getFiles() {
+		return files;
+	}
+
+	public void setFiles(ArrayList<File> files) {
+		this.files = files;
+	}
+
+	public int getUploadsAvailable() {
+		return uploadsAvailable;
+	}
+
+	public void setUploadsAvailable(int uploadsAvailable) {
+		this.uploadsAvailable = uploadsAvailable;
+	}
+
+	public boolean isAutoUpload() {
+		return autoUpload;
+	}
+
+	public void setAutoUpload(boolean autoUpload) {
+		this.autoUpload = autoUpload;
+	}
+
+	public boolean isUseFlash() {
+		return useFlash;
+	}
+
+	public void setUseFlash(boolean useFlash) {
+		this.useFlash = useFlash;
+	}
 
 }
