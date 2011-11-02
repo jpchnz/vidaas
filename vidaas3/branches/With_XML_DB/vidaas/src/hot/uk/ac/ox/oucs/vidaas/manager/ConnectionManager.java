80b7fcff43a3046a0ebe863ec63348d2bf8dd61f
thestoat <thestoat@gmail.com>
2011-11-02 10:08:25 +0000

    Backed out previous changes

--- vidaas3/branches/With_XML_DB/vidaas/src/hot/uk/ac/ox/oucs/vidaas/manager/ConnectionManager.java
+++ vidaas3/branches/With_XML_DB/vidaas/src/hot/uk/ac/ox/oucs/vidaas/manager/ConnectionManager.java
@@ -9,152 +9,127 @@ import java.sql.*;
 import java.util.Properties;
 
 /**
- * 
+ *
  * @author Asif Akram
  */
 public class ConnectionManager {
 
-	private String adminUserName = null; // "sudamihadmin";
-	private String adminUserNamePW = null; // "sudamihAdminPW";
-	private String databaseName = null; // "sudamihtestdb";
-	private String driverName = null; // "org.postgresql.Driver";
-	private String databaseURL = null;
-	private Connection connection = null;
-	/*
-	 * The following vars not currently used
-	 */
-	// private ResultSet rs = null;
-	// private Statement st = null;
-	private static Logger logger = Logger.getLogger(ConnectionManager.class
-			.getName());
+    private String adminUserName = null; // "sudamihadmin";
+    private String adminUserNamePW = null; // "sudamihAdminPW";
+    private String databaseName = null; // "sudamihtestdb";
+    private String driverName = null; // "org.postgresql.Driver";
+    private String databaseURL = null;
+    private Connection connection = null;
+    private ResultSet rs = null;
+    private Statement st = null;
 
-	public ConnectionManager() {
-		logger.log(Level.INFO, "ConnectionManager constructor");
-	}
+    public ConnectionManager(){
+        Logger.getLogger(ConnectionManager.class.getName()).log(Level.SEVERE, null, "Default Constructor called");
+    }
 
-	private void readPropertiesFile() {
-		logger.log(Level.INFO, "Read properties file");
-		Properties props = new Properties();
-		try {
-			InputStream in = this.getClass().getResourceAsStream(
-					"sudamih.properties");
-			props.load(in);
-			in.close();
-			this.adminUserName = props
-					.getProperty("uk.ac.ox.oucs.sudamih.adminUserName");
-			this.adminUserNamePW = props
-					.getProperty("uk.ac.ox.oucs.sudamih.adminUserNamePW");
-			this.databaseName = props
-					.getProperty("uk.ac.ox.oucs.sudamih.databaseName");
-			this.driverName = props
-					.getProperty("uk.ac.ox.oucs.sudamih.driverName");
-			this.databaseURL = props
-					.getProperty("uk.ac.ox.oucs.sudamih.databaseURL");
-		} catch (IOException ex) {
-			logger.log(Level.SEVERE, "IOException", ex);
-		}
+    private void readPropertiesFile() {
+        Properties props = new Properties();
+        try {
+            InputStream in = this.getClass().getResourceAsStream("sudamih.properties");
+            props.load(in);
+            in.close();
+            this.adminUserName = props.getProperty("uk.ac.ox.oucs.sudamih.adminUserName");
+            this.adminUserNamePW = props.getProperty("uk.ac.ox.oucs.sudamih.adminUserNamePW");
+            this.databaseName = props.getProperty("uk.ac.ox.oucs.sudamih.databaseName");
+            this.driverName = props.getProperty("uk.ac.ox.oucs.sudamih.driverName");
+            this.databaseURL = props.getProperty("uk.ac.ox.oucs.sudamih.databaseURL");
+        } catch (IOException ex) {
+            Logger.getLogger(ConnectionManager.class.getName()).log(Level.SEVERE, null, ex);
+        }
 
-		// System.out.println(this.adminUserName + " " + this.adminUserNamePW
-		// + " " + this.databaseName + " " + this.driverName + " "
-		// + this.databaseURL);
-		if (logger.isLoggable(Level.INFO)) {
-			logger.log(Level.INFO, String.format(
-					"Details: admin=%s, db=%s, driver=%s, url=%s",
-					adminUserName, databaseName, driverName, databaseURL));
-		}
+        System.out.println(this.adminUserName + " " + this.adminUserNamePW
+                + " " + this.databaseName + " " + this.driverName + " " + this.databaseURL);
 
-	}
+    }
 
-	private Connection createConnection() {
-		Connection connectionTemp = null;
-		readPropertiesFile();
-		try {
-			Class.forName(driverName);
-			connectionTemp = DriverManager.getConnection(databaseURL
-					+ databaseName, adminUserName, adminUserNamePW);
-		} catch (ClassNotFoundException ex) {
-			logger.log(Level.SEVERE, "ClassNotFoundException", ex);
-		} catch (SQLException ex) {
-			logger.log(Level.SEVERE, "SQLException", ex);
-		}
+    private Connection createConnection() {
+        Connection connectionTemp= null;
+        readPropertiesFile();
+        try {
+            Class.forName(driverName);    
+            connectionTemp = DriverManager.getConnection(databaseURL + databaseName, adminUserName, adminUserNamePW);
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(ConnectionManager.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (SQLException ex) {
+            Logger.getLogger(this.getClass().getName()).log(Level.SEVERE, null, ex);
+        }
 
-		return connectionTemp;
-	}
+        return connectionTemp;
+    }
 
-	/**/
-	public Connection createConnection(String databaseNameValue) {
-		Connection connectionTemp = null;
-		readPropertiesFile();
-		this.databaseName = databaseNameValue;
-		System.out.println(this.adminUserName + " " + this.adminUserNamePW
-				+ " " + this.databaseName + " " + this.driverName + " "
-				+ this.databaseURL);
-		try {
-			Class.forName(driverName);
+    /**/
+    public Connection createConnection(String databaseNameValue) {
+        Connection connectionTemp= null;
+        readPropertiesFile();
+        this.databaseName = databaseNameValue;
+        System.out.println(this.adminUserName + " " + this.adminUserNamePW
+                + " " + this.databaseName + " " + this.driverName + " " + this.databaseURL);
+        try {
+            Class.forName(driverName);
 
-			// Only for testing purposes ...!
-			// con = DriverManager.getConnection("jdbc:postgresql:" + dbName,
-			// "postgres", "bna192");
+            // Only for testing purposes ...!
+            //con = DriverManager.getConnection("jdbc:postgresql:" + dbName, "postgres", "bna192");
 
-			connectionTemp = DriverManager.getConnection(databaseURL
-					+ databaseName, adminUserName, adminUserNamePW);
-		} catch (ClassNotFoundException ex) {
-			logger.log(Level.SEVERE, "ClassNotFoundException", ex);
-		} catch (SQLException ex) {
-			logger.log(Level.SEVERE, "SQLException", ex);
-		}
+            connectionTemp = DriverManager.getConnection(databaseURL + databaseName, adminUserName, adminUserNamePW);
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(ConnectionManager.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (SQLException ex) {
+            Logger.getLogger(this.getClass().getName()).log(Level.SEVERE, null, ex);
+        }
 
-		return connectionTemp;
-	}
+        return connectionTemp;
+    }
+    
+    public Connection createConnection(String databaseNameValue, String userNameValue, String passwordValue) {
+        Connection connectionTemp= null;
+        readPropertiesFile();
+        //this.databaseName = databaseNameValue;
+        /*
+        System.out.println(this.adminUserName + " " + this.adminUserNamePW
+                + " " + this.databaseName + " " + this.driverName + " " + this.databaseURL); */
+        try {
+            Class.forName(driverName);
 
-	public Connection createConnection(String databaseNameValue,
-			String userNameValue, String passwordValue) {
-		Connection connectionTemp = null;
-		readPropertiesFile();
-		// this.databaseName = databaseNameValue;
-		/*
-		 * System.out.println(this.adminUserName + " " + this.adminUserNamePW +
-		 * " " + this.databaseName + " " + this.driverName + " " +
-		 * this.databaseURL);
-		 */
-		try {
-			Class.forName(driverName);
+            // Only for testing purposes ...!
+            //con = DriverManager.getConnection("jdbc:postgresql:" + dbName, "postgres", "bna192");
 
-			// Only for testing purposes ...!
-			// con = DriverManager.getConnection("jdbc:postgresql:" + dbName,
-			// "postgres", "bna192");
+            connectionTemp = DriverManager.getConnection(databaseURL + databaseNameValue, userNameValue, passwordValue);
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(ConnectionManager.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (SQLException ex) {
+            Logger.getLogger(this.getClass().getName()).log(Level.SEVERE, null, ex);
+        }
 
-			connectionTemp = DriverManager.getConnection(databaseURL
-					+ databaseNameValue, userNameValue, passwordValue);
-		} catch (ClassNotFoundException ex) {
-			logger.log(Level.SEVERE, "ClassNotFoundException", ex);
-		} catch (SQLException ex) {
-			logger.log(Level.SEVERE, "SQLException", ex);
-		}
+        return connectionTemp;
+    }
 
-		return connectionTemp;
-	}
+    public Connection getConnection(){
+        if (connection == null){
+            connection = createConnection();
+        }
+        return connection;
+    }
 
-	public Connection getConnection() {
-		if (connection == null) {
-			connection = createConnection();
-		}
-		return connection;
-	}
+    public String getDatabaseName() {
+        return databaseName;
+    }
 
-	public String getDatabaseName() {
-		return databaseName;
-	}
+    public void setDatabaseName(String databaseName) {
+        this.databaseName = databaseName;
+    }
 
-	public void setDatabaseName(String databaseName) {
-		this.databaseName = databaseName;
-	}
 
-	public static void main(String argv[]) {
-		ConnectionManager cm = new ConnectionManager();
-		cm.getConnection();
-		if (cm.connection != null) {
-			System.out.println("Connection created Successfully ...!");
-		}
-	}
+ 
+    public static void main(String argv[]) {
+    	ConnectionManager cm = new ConnectionManager();
+        cm.getConnection();
+         if (cm.connection != null){
+             System.out.println("Connection created Successfully ...!");
+         }
+    }
 }
