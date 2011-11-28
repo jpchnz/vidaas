35d983916225f5997426f9bc242bd8b26a74c613
aassif.akram <aassif.akram@googlemail.com>
2011-11-28 17:14:32 +0000

    Supporting Web Application Development

---
+++ vidaas3/trunk/vidaas/src/hot/uk/ac/ox/oucs/vidaas/concurrency/CreateWebApplicationThread.java
@@ -0,0 +1,41 @@
+package uk.ac.ox.oucs.vidaas.concurrency;
+
+import uk.ac.ox.oucs.vidaas.create.CreateWebApplication;
+import uk.ac.ox.oucs.vidaas.data.holder.DataHolder;
+
+public class CreateWebApplicationThread  implements Runnable {
+	
+	private DataHolder dataHolder = null;
+	
+	private String webApplicationName;
+	private String databaseName;
+	private String userName;
+	private String password;
+	private String webApplicationLocation;
+	
+	public CreateWebApplicationThread(String webApplicationNameValue, String webApplicationLocationValue, 
+			String databaseNameValue, String userNameValue, String passwordValue, DataHolder dataHolderValue){
+		
+		this.webApplicationName = webApplicationNameValue;
+		this.webApplicationLocation = webApplicationLocationValue;
+		this.databaseName = databaseNameValue;
+		this.userName = userNameValue;
+		this.password = passwordValue;
+		
+		dataHolder = dataHolderValue;
+	}
+
+	public void run(){
+		
+		String serverURLTemp = System.getProperty("serverURL");
+		
+		dataHolder.setOkButton(true);
+		
+		CreateWebApplication createWebApplication = new CreateWebApplication();
+		createWebApplication.createWebApplication(webApplicationName,
+				webApplicationLocation, databaseName, userName, password, dataHolder);
+		
+		dataHolder.setCurrentStatus("Web Application will be available at: '" + serverURLTemp + webApplicationName + "' after few minutes");
+		dataHolder.setOkButton(false);
+	}
+}
