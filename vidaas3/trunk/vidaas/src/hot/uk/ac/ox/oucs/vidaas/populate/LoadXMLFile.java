703de03b962c41b7782d43fc361f4d3c77f62684
aassif.akram@googlemail.com <aassif.akram@googlemail.com@24beba35-015c-934d-eeb6-620b1e2b2385>
2011-11-28 17:15:24 +0000

    Supporting Web Application Development

---
+++ vidaas3/trunk/vidaas/src/hot/uk/ac/ox/oucs/vidaas/populate/LoadXMLFile.java
@@ -0,0 +1,13 @@
+package uk.ac.ox.oucs.vidaas.populate;
+
+import java.io.*;
+import java.util.*;
+
+import com.sleepycat.db.*;
+import com.sleepycat.dbxml.*;
+
+public class LoadXMLFile {
+	
+	
+
+}
