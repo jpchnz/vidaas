748257d970d804bd4ad98f0c547a54e7b76a4be8
thestoat@gmail.com <thestoat@gmail.com@24beba35-015c-934d-eeb6-620b1e2b2385>
2012-01-24 15:28:05 +0000

    Commented all code temporarily

--- vidaas3/trunk/vidaas/src/hot/uk/ac/ox/oucs/vidaas/session/HelpController.java
+++ vidaas3/trunk/vidaas/src/hot/uk/ac/ox/oucs/vidaas/session/HelpController.java
@@ -9,37 +9,37 @@ import uk.ac.ox.oucs.vidaas.session.generated.HelpField;
 @Name("helpController")
 @Scope(ScopeType.SESSION)
 public class HelpController {
-	private HelpTextGenerator helpTextGenerator;
-	private String helpId = null;
-	
-	public HelpController() {
-		try {
-			helpTextGenerator = new HelpTextGenerator();
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
-	
-	public String setupHelpText(String helpId) {
-		HelpField helpField = helpTextGenerator.getHelpField(helpId);
-		if (helpField == null) {
-			return "No help available";
-		}
-		this.helpId = helpId;
-		return helpField.getText();
-	}
-
-	public String getHelpText() {
-		if (helpId == null) {
-			return "No help available";
-		}
-		HelpField helpField = helpTextGenerator.getHelpField(helpId);
-		return helpField.getText();
-	}
-
-	public void setHelpId(String helpId) {
-		this.helpId = helpId;
-	}
+//	private HelpTextGenerator helpTextGenerator;
+//	private String helpId = null;
+//	
+//	public HelpController() {
+//		try {
+//			helpTextGenerator = new HelpTextGenerator();
+//		} catch (Exception e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+//	}
+//	
+//	public String setupHelpText(String helpId) {
+//		HelpField helpField = helpTextGenerator.getHelpField(helpId);
+//		if (helpField == null) {
+//			return "No help available";
+//		}
+//		this.helpId = helpId;
+//		return helpField.getText();
+//	}
+//
+//	public String getHelpText() {
+//		if (helpId == null) {
+//			return "No help available";
+//		}
+//		HelpField helpField = helpTextGenerator.getHelpField(helpId);
+//		return helpField.getText();
+//	}
+//
+//	public void setHelpId(String helpId) {
+//		this.helpId = helpId;
+//	}
 
 }
