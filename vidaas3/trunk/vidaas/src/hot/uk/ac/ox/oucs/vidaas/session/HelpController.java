576be4f0cc4976e006b0a270593af9ebfd95932b
thestoat@gmail.com <thestoat@gmail.com@24beba35-015c-934d-eeb6-620b1e2b2385>
2012-02-01 13:53:55 +0000

    In-context help system

--- vidaas3/trunk/vidaas/src/hot/uk/ac/ox/oucs/vidaas/session/HelpController.java
+++ vidaas3/trunk/vidaas/src/hot/uk/ac/ox/oucs/vidaas/session/HelpController.java
@@ -1,46 +1,66 @@
 package uk.ac.ox.oucs.vidaas.session;
 
+import java.io.FileNotFoundException;
+
+import javax.xml.bind.JAXBException;
+
 import org.jboss.seam.ScopeType;
 import org.jboss.seam.annotations.Name;
 import org.jboss.seam.annotations.Scope;
 
-//import uk.ac.ox.oucs.vidaas.session.generated.HelpField;
+import uk.ac.ox.oucs.vidaas.session.generated.HelpField;
 
-//@Name("helpController")
-//@Scope(ScopeType.SESSION)
+@Name("helpController")
+@Scope(ScopeType.SESSION)
 public class HelpController {
-//	private HelpTextGenerator helpTextGenerator;
-//	private String helpId = null;
-//	
-//	public HelpController() {
-//		try {
-//			helpTextGenerator = new HelpTextGenerator();
-//		} catch (Exception e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}
-//	}
-//	
-	public String setupHelpText(String helpId) {
-//		HelpField helpField = helpTextGenerator.getHelpField(helpId);
-//		if (helpField == null) {
-//			return "No help available";
-//		}
-//		this.helpId = helpId;
-//		return helpField.getText();
-		return "Here is some help text for id " + helpId;
+	private HelpTextGenerator helpTextGenerator;
+	private String helpId = null;
+	
+	public HelpController() {
+		try {
+			helpTextGenerator = new HelpTextGenerator();
+		}
+		catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		catch (JAXBException e) {
+			e.printStackTrace();
+		}
 	}
-//
-//	public String getHelpText() {
-//		if (helpId == null) {
-//			return "No help available";
-//		}
-//		HelpField helpField = helpTextGenerator.getHelpField(helpId);
-//		return helpField.getText();
-//	}
-//
-//	public void setHelpId(String helpId) {
-//		this.helpId = helpId;
-//	}
+	
+	public String setupHelpText(String helpId) {
+		if (helpTextGenerator == null) {
+			return "No help available";
+		}
+		HelpField helpField = helpTextGenerator.getHelpField(helpId);
+		if (helpField == null) {
+			return "No help available";
+		}
+		this.helpId = helpId;
+		return helpField.getText();
+	}
 
+	public String getHelpText() {
+		if ( (helpId == null) || (helpTextGenerator == null) ) {
+			return "No help available";
+		}
+		HelpField helpField = helpTextGenerator.getHelpField(helpId);
+		return helpField.getText();
+	}
+	
+	public String getHelpTitle(String helpId) {
+		if (helpTextGenerator == null) {
+			return "No help available";
+		}
+		HelpField helpField = helpTextGenerator.getHelpField(helpId);
+		if (helpField == null) {
+			return "No help available";
+		}
+		this.helpId = helpId;
+		return helpField.getTitle();
+	}
+
+	public void setHelpId(String helpId) {
+		this.helpId = helpId;
+	}
 }
---
+++ vidaas3/trunk/vidaas/src/hot/uk/ac/ox/oucs/vidaas/session/HelpTextGenerator.java
@@ -0,0 +1,100 @@
+package uk.ac.ox.oucs.vidaas.session;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.List;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Unmarshaller;
+
+import uk.ac.ox.oucs.vidaas.session.generated.HelpField;
+import uk.ac.ox.oucs.vidaas.session.generated.HelpGroup;
+import uk.ac.ox.oucs.vidaas.session.generated.HelpText;
+
+public class HelpTextGenerator {
+	private HelpText helpText;
+	private File helpFile;
+	boolean helpEnabled = true;
+
+	public HelpTextGenerator() throws JAXBException, FileNotFoundException {
+		try {
+			loadHelpFile();
+		}
+		catch (IOException e) {
+			helpFile = new File("/tmp/helpText.xml");
+		}
+		
+		init();
+	}
+
+	private void loadHelpFile() throws IOException {
+		InputStream inputStream = HelpTextGenerator.class.getClassLoader().getResourceAsStream("helpText.xml");
+		if (inputStream == null) {
+			throw new IOException();
+		}
+		helpFile = new File("helpfile");
+		OutputStream out = new FileOutputStream(helpFile);
+		byte buf[] = new byte[1024];
+		int len;
+		while ((len = inputStream.read(buf)) > 0) {
+			out.write(buf, 0, len);
+		}
+		out.close();
+		inputStream.close();
+	}
+
+	public HelpTextGenerator(String fileName) throws JAXBException {
+		helpFile = new File(fileName);
+		init();
+	}
+
+	private void init() throws JAXBException {
+		if (!helpFile.exists()) {
+			System.out.println("Unable to find help file " + helpFile.getAbsolutePath()
+					+ ". Help will not be available.");
+			helpEnabled = false;
+		}
+		JAXBContext jaxbContext = JAXBContext.newInstance(HelpText.class);
+		Unmarshaller jaxbUnmarshaller = jaxbContext.createUnmarshaller();
+		helpText = (HelpText) jaxbUnmarshaller.unmarshal(helpFile);
+	}
+
+	public HelpField getHelpField(String helpId) {
+		if (!helpEnabled) {
+			return null;
+		}
+
+		List<HelpGroup> helpGroupList = helpText.getHelpGroup();
+		for (int counter = 0; counter < helpGroupList.size(); counter++) {
+			List<HelpField> helpFieldList = helpGroupList.get(counter).getHelpField();
+			for (int counter2 = 0; counter2 < helpFieldList.size(); counter2++) {
+				if (helpFieldList.get(counter2).getHelpId().compareToIgnoreCase(helpId) == 0) {
+					return helpFieldList.get(counter2);
+				}
+			}
+		}
+		return null;
+	}
+
+	public static void main(String args[]) {
+		try {
+			HelpTextGenerator helpTextGenerator = new HelpTextGenerator();
+			HelpField helpField = helpTextGenerator.getHelpField("CreateDataSpacePopup_Field02");
+			if (helpField != null) {
+				System.out.println(helpField.getText());
+			}
+			else {
+				System.out.println("No help available");
+			}
+		}
+		catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
