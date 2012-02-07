2b8cef1ecd81407a383fff7ca24e25e49c360f02
thestoat <thestoat@gmail.com>
2012-02-07 10:25:28 +0000

    Updated test cases

--- iam/src/test/java/uk/ac/ox/oucs/iam/postsecurely/SendAndReceiveTest.java
+++ iam/src/test/java/uk/ac/ox/oucs/iam/postsecurely/SendAndReceiveTest.java
@@ -33,16 +33,32 @@ public class SendAndReceiveTest {
 			return;
 		}
 		System.out.println("test1");
+		
+		System.out.println("\tcheck send data ...");
 		SendViaPost post = new SendViaPost();
-		post.sendPost(webAppName, "name=freddy&password=bibble");
+		
+		
+		/*
+		 * First we send some data to the web server ...
+		 */
+		String postedData = post.sendPost(webAppName, "name=freddy&password=bibble");
+		assertFalse(postedData == null);
+		System.out.println("\tData posted <" + postedData + ">");
+		
+		
+		/*
+		 * Now try to receive it
+		 */
 		List<SecurePostData> securePostDataList = null;
+		System.out.println("\tcheck receive data ...");
 		int counter = 10;
 		while ( (securePostDataList == null) && (counter != 0) ) {
-			Thread.sleep(1000);
+			Thread.sleep(1000); // We need to wait a second or so to ensure the data has been received and processed ...
 			securePostDataList = ReceivePostedData.getPendingMessageData(webAppName);
 			counter--;
 		}
 		assertTrue(counter > 0); // Timeout waiting for data
+		System.out.println("\tTotal extra wait was " + (9-counter) + " seconds");
 		assertFalse(securePostDataList == null);
 		assertTrue(securePostDataList.size()==1);
 		assertTrue(securePostDataList.get(0).isMessageHasBeenVerified());
@@ -52,6 +68,7 @@ public class SendAndReceiveTest {
 		assertTrue(securePostDataList.get(0).getPostParms().size() == 2);
 		assertTrue(securePostDataList.get(0).getPostParms().get(0).compareTo("password=bibble") == 0);
 		assertTrue(securePostDataList.get(0).getPostParms().get(1).compareTo("name=freddy") == 0);
+		System.out.println("\tData returned and understood successfully");
 	}
 	
 	
@@ -65,10 +82,17 @@ public class SendAndReceiveTest {
 			return;
 		}
 		System.out.println("test2");
+		
+		System.out.println("\tcheck send data ...");
 		SendViaPost post = new SendViaPost();
-		post.sendPost(webAppName, "name=Another name&password=This Is My Password123");
-		post.sendPost(webAppName, "name=Another name&password=This Is My Password123");
+		String postedData = post.sendPost(webAppName, "name=Another name is here&password=My Secret Passwordzzz123");
+		assertFalse(postedData == null);
+		System.out.println("\tData posted <" + postedData + ">");
+		postedData = post.sendPost(webAppName, "name=Another name&password=This Is My Password123");
+		assertFalse(postedData == null);
+		System.out.println("\tData posted <" + postedData + ">");
 		List<SecurePostData> securePostDataList = null;
+		System.out.println("\tcheck receive data ...");
 		int counter = 10;
 		while ( (securePostDataList == null) && (counter != 0) ) {
 			Thread.sleep(1000);
@@ -76,6 +100,7 @@ public class SendAndReceiveTest {
 			counter--;
 		}
 		assertTrue(counter > 0); // Timeout waiting for data
+		System.out.println("\tTotal extra wait was " + (9-counter) + " seconds");
 		assertFalse(securePostDataList == null);
 		assertTrue(securePostDataList.size()==2);
 		assertTrue(securePostDataList.get(0).isMessageHasBeenVerified());
@@ -83,7 +108,10 @@ public class SendAndReceiveTest {
 		assertFalse(securePostDataList.get(0).isMessageTimedOut());
 		assertFalse(securePostDataList.get(0).isNoPrivateKey());
 		assertTrue(securePostDataList.get(0).getPostParms().size() == 2);
-		assertTrue(securePostDataList.get(0).getPostParms().get(0).compareTo("password=This Is My Password123") == 0);
-		assertTrue(securePostDataList.get(0).getPostParms().get(1).compareTo("name=Another name") == 0);
+		assertTrue(securePostDataList.get(0).getPostParms().get(0).compareTo("password=My Secret Passwordzzz123") == 0);
+		assertTrue(securePostDataList.get(0).getPostParms().get(1).compareTo("name=Another name is here") == 0);
+		assertTrue(securePostDataList.get(1).getPostParms().get(0).compareTo("password=This Is My Password123") == 0);
+		assertTrue(securePostDataList.get(1).getPostParms().get(1).compareTo("name=Another name") == 0);
+		System.out.println("\tData returned and understood successfully");
 	}
 }
