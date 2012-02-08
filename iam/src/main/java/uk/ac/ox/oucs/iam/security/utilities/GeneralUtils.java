7fac9743d8a4997d006401667ad1669c7ed4c436
thestoat <thestoat@gmail.com>
2012-02-08 11:43:45 +0000

    Code to look at key distribution

--- iam/src/main/java/uk/ac/ox/oucs/iam/security/utilities/GeneralUtils.java
+++ iam/src/main/java/uk/ac/ox/oucs/iam/security/utilities/GeneralUtils.java
@@ -1,8 +1,10 @@
 package uk.ac.ox.oucs.iam.security.utilities;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.ObjectInputStream;
@@ -164,6 +166,22 @@ public class GeneralUtils {
 
 		return o;
 	}
+	
+	public static String readFileAsString(String filePath)
+		    throws java.io.IOException{
+		        StringBuffer fileData = new StringBuffer(1000);
+		        BufferedReader reader = new BufferedReader(
+		                new FileReader(filePath));
+		        char[] buf = new char[1024];
+		        int numRead=0;
+		        while((numRead=reader.read(buf)) != -1){
+		            String readData = String.valueOf(buf, 0, numRead);
+		            fileData.append(readData);
+		            buf = new char[1024];
+		        }
+		        reader.close();
+		        return fileData.toString();
+		    }
 
 	/**
 	 * Create a file with specific contents (such as a key)
