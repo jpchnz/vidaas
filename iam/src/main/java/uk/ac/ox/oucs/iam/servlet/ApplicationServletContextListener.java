27540370f79cb875d39dfc2a2f8527e9eec0a490
thestoat@gmail.com <thestoat@gmail.com@24beba35-015c-934d-eeb6-620b1e2b2385>
2012-02-08 15:29:41 +0000

    Code to look at key distribution

--- iam/src/main/java/uk/ac/ox/oucs/iam/servlet/ApplicationServletContextListener.java
+++ iam/src/main/java/uk/ac/ox/oucs/iam/servlet/ApplicationServletContextListener.java
@@ -13,10 +13,12 @@ import javax.servlet.ServletContextListener;
 
 import org.apache.log4j.xml.DOMConfigurator;
 
+import uk.ac.ox.oucs.iam.postsecurely.SendViaPost;
 import uk.ac.ox.oucs.iam.security.keys.KeyServices;
 import uk.ac.ox.oucs.iam.security.utilities.GeneralUtils;
 import uk.ac.ox.oucs.iam.security.utilities.exceptions.DuplicateKeyException;
 import uk.ac.ox.oucs.iam.security.utilities.exceptions.KeyNotFoundException;
+import uk.ac.ox.oucs.iam.security.utilities.exceptions.NewKeyException;
 
 public class ApplicationServletContextListener implements ServletContextListener {
 	public void contextInitialized(ServletContextEvent event) {
@@ -24,7 +26,7 @@ public class ApplicationServletContextListener implements ServletContextListener
 		 * TODO
 		 * Send a ping to the manager node so that it knows where to send public keys to.
 		 */
-		prepareAndSendPublicKey();
+		prepareAndSendPublicKey(true); // Set to false to actually send the text
 		
 		
 		/*
@@ -39,11 +41,11 @@ public class ApplicationServletContextListener implements ServletContextListener
 		DOMConfigurator.configure(prefix + file);
 	}
 	
-	private void prepareAndSendPublicKey() {
+	private String prepareAndSendPublicKey(boolean prepareStringOnly) {
 		String publicKey;
 		String hostName = GeneralUtils.getLocalHostname();
 		String ip = GeneralUtils.getLocalIPAddress();
-		String ipAddressOfManager = "manager"; // TODO This should be via a configuration file
+		String ipAddressOfManager = "localhost"; // TODO This should be via a configuration file
 		String dataToPost = null;
 		
 		try {
@@ -51,10 +53,13 @@ public class ApplicationServletContextListener implements ServletContextListener
 			dataToPost = String.format("%s=%s&%s=%s&%s=%s&%s=%s",
 					KeyUtilitiesServlet.HELLO_WORLD_ATTRIBUTE, ip,
 					KeyUtilitiesServlet.HELLO_WORLD_HOSTNAME_ATTRIBUTE, hostName,
-					KeyUtilitiesServlet.HELLO_WORLD_PUBLICKEYNAME_ATTRIBUTE, new File(GeneralUtils.provideBaseKeyPairName()).getName(),
-					KeyUtilitiesServlet.HELLO_WORLD_PUBLICKEY_ATTRIBUTE, GeneralUtils.readFileAsString(publicKey)); // TODO - needs encoding
-			System.out.println(publicKey);
-			URL url = new URL("http://" + ipAddressOfManager + ":8081/iam/KeyUtilitiesServlet");
+					KeyUtilitiesServlet.HELLO_WORLD_PUBLICKEYNAME_ATTRIBUTE, new File(GeneralUtils.provideBaseKeyPairName()).getName()+"xx",
+					KeyUtilitiesServlet.HELLO_WORLD_PUBLICKEY_ATTRIBUTE, GeneralUtils.readPublicKeyFromFileAndEncode(publicKey)); // TODO - needs encoding
+			System.out.println(dataToPost);
+			if (prepareStringOnly) {
+				return dataToPost;
+			}
+			URL url = new URL("http://" + ipAddressOfManager + ":8081/iam/KeyUtilitiesServlet");//http://localhost:8081/iam
 			URLConnection connection = url.openConnection();
 			connection.setDoOutput(true);
 			OutputStreamWriter outputSW = new OutputStreamWriter(connection.getOutputStream());
@@ -80,9 +85,39 @@ public class ApplicationServletContextListener implements ServletContextListener
 		}
 		
 		System.out.println(dataToPost);
+		return dataToPost;
 	}
 
 	public void contextDestroyed(ServletContextEvent event) {
 	}
 
+	public static void main(String[] args) {
+		ApplicationServletContextListener thisObject = new ApplicationServletContextListener();
+		String postData = thisObject.prepareAndSendPublicKey(true);
+		SendViaPost post = new SendViaPost();
+		try {
+			post.sendPost("http://129.67.103.124:8081/iam/KeyUtilitiesServlet", postData, false);
+		}
+		catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		catch (NewKeyException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		catch (KeyNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		catch (DuplicateKeyException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		/*
+		 * Example output
+		 * helloWorld=129.67.103.124&hostName=ethics-gradient&publicKeyName=eba95ef3-9fcd-44c2-88ba-d9f1f3172446&publicKey=MIIBuDCCASwGByqGSM44BAEwggEfAoGBAP1/U4EddRIpUt9KnC7s5Of2EbdSPO9EAMMeP4C2USZpRV1AIlH7WT2NWPq/xfW6MPbLm1Vs14E7gB00b/JmYLdrmVClpJ+f6AR7ECLCT7up1/63xhv4O1fnxqimFQ8E+4P208UewwI1VBNaFpEy9nXzrith1yrv8iIDGZ3RSAHHAhUAl2BQjxUjC8yykrmCouuEC/BYHPUCgYEA9+GghdabPd7LvKtcNrhXuXmUr7v6OuqC+VdMCz0HgmdRWVeOutRZT+ZxBxCBgLRJFnEj6EwoFhO3zwkyjMim4TwWeotUfI0o4KOuHiuzpnWRbqN/C/ohNWLx+2J6ASQ7zKTxvqhRkImog9/hWuWfBpKLZl6Ae1UlZAFMO/7PSSoDgYUAAoGBAJxjLg0EDP5u3fPbv/Y5dP4Dw5al6ig+FJbF5k++zaXT1RebFIR9zbUZG/Lt1Hh4ILL+PTqXlV7qnEZ9vzTltENEJvWZtuH3s3jMJ7CFgZyvv1L+1b64OGX7OUCvOPdB0wrQaKJG8b5Y50HClqiWDWily9RH7KDwn1GkyktlFWE7
+		 */
+	}
 }
