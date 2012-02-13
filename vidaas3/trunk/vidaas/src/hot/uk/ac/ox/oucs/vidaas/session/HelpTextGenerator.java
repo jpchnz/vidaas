4d09179a5a32cef6a02a5bf299c722805525f78c
thestoat@gmail.com <thestoat@gmail.com@24beba35-015c-934d-eeb6-620b1e2b2385>
2012-02-13 11:14:18 +0000

    Fixed possible npe with in-context help text

--- vidaas3/trunk/vidaas/src/hot/uk/ac/ox/oucs/vidaas/session/HelpTextGenerator.java
+++ vidaas3/trunk/vidaas/src/hot/uk/ac/ox/oucs/vidaas/session/HelpTextGenerator.java
@@ -1,99 +1,77 @@
 package uk.ac.ox.oucs.vidaas.session;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
 import java.io.InputStream;
-import java.io.OutputStream;
 import java.util.List;
 
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Unmarshaller;
 
-import org.jboss.seam.log.Log;
-
 import uk.ac.ox.oucs.vidaas.session.generated.HelpField;
 import uk.ac.ox.oucs.vidaas.session.generated.HelpGroup;
 import uk.ac.ox.oucs.vidaas.session.generated.HelpText;
 
+/**
+ * HelpTextGenerator prepares a HelpText object and populates it with data from the
+ * htlpText.xml resource file. It is then able to provide help text for specific help ids. 
+ * 
+ * @author oucs0153
+ *
+ */
 public class HelpTextGenerator {
 	private HelpText helpText;
-	private File helpFile;
-	boolean helpEnabled = true;
-	private Log log;
+	private boolean helpEnabled = true;
+	private InputStream inputStream;
 
-	public HelpTextGenerator(Log log) throws JAXBException, FileNotFoundException {
-		this.log = log;
-		try {
-			loadHelpFile();
+	
+	/**
+	 * Prepare the help text generator.
+	 * 
+	 * @throws JAXBException
+	 */
+	public HelpTextGenerator() throws JAXBException {
+		helpEnabled = loadHelpFile();
+		if (helpEnabled) {
+			init();
 		}
-		catch (IOException e) {
-			log.error("Unable to find helpfile in the standard place");
-			helpFile = new File("/tmp/helpText.xml");
+		else {
+			System.out.println("Unable to prepare in context help");
 		}
-		
-		init();
+	}
+
+	/**
+	 * Find the helpText xml file and prepare an input stream for this resource for JAXB.
+	 * 
+	 * @return true if was able to read the help text as a resource, else false
+	 */
+	private boolean loadHelpFile() {
+		boolean ret = true;
+		inputStream = HelpTextGenerator.class.getClassLoader().getResourceAsStream("helpText.xml");
+		if (inputStream == null) {
+			System.out.println("Unable to get help file resource");
+			ret = false;
+		}
+		System.out.println("We have our input stream");
+		return ret;
+	}
+
+
+	/**
+	 * Initialise the help text object using JAXB
+	 * 
+	 * @throws JAXBException
+	 */
+	private void init() throws JAXBException {
+		JAXBContext jaxbContext = JAXBContext.newInstance(HelpText.class);
+		Unmarshaller jaxbUnmarshaller = jaxbContext.createUnmarshaller();
+		helpText = (HelpText) jaxbUnmarshaller.unmarshal(inputStream);
 	}
 	
-	private HelpTextGenerator() throws JAXBException, FileNotFoundException {
-		try {
-			loadHelpFile();
-		}
-		catch (IOException e) {
-			System.out.println("Unable to find helpfile in the standard place");
-			helpFile = new File("/tmp/helpText.xml");
-		}
-		
-		init();
-	}
 
-	private void loadHelpFile() throws IOException {
-		InputStream inputStream = HelpTextGenerator.class.getClassLoader().getResourceAsStream("helpText.xml");
-		if (inputStream == null) {
-			if (log == null) {
-				System.out.println("Unable to get resource");
-			}
-			else {
-				log.error("Unable to get resource");
-			}
-			throw new IOException();
-		}
-		helpFile = new File("helpfile");
-		OutputStream out = new FileOutputStream(helpFile);
-		byte buf[] = new byte[1024];
-		int len;
-		while ((len = inputStream.read(buf)) > 0) {
-			out.write(buf, 0, len);
-		}
-		out.close();
-		inputStream.close();
-		if (log == null) {
-			System.out.println("Help text has been loaded");
-		}
-		else {
-			log.error("Help text has been loaded");
-		}
-	}
-
-	public HelpTextGenerator(String fileName) throws JAXBException {
-		helpFile = new File(fileName);
-		init();
-	}
-
-	private void init() throws JAXBException {
-		if (!helpFile.exists()) {
-			System.out.println("Unable to find help file. Help will not be available.");
-			helpEnabled = false;
-		}
-		else {
-			JAXBContext jaxbContext = JAXBContext.newInstance(HelpText.class);
-			Unmarshaller jaxbUnmarshaller = jaxbContext.createUnmarshaller();
-			helpText = (HelpText) jaxbUnmarshaller.unmarshal(helpFile);
-		}
-	}
-
+	/**
+	 * @param helpId the id of the HelpField object required
+	 * @return the HelpField object that relates to the required id, or null if help is not availabe 
+	 */
 	public HelpField getHelpField(String helpId) {
 		if (!helpEnabled) {
 			return null;
@@ -114,6 +92,7 @@ public class HelpTextGenerator {
 	public static void main(String args[]) {
 		try {
 			HelpTextGenerator helpTextGenerator = new HelpTextGenerator();
+			System.out.println(helpTextGenerator.helpEnabled);
 			HelpField helpField = helpTextGenerator.getHelpField("CreateDataSpacePopup_Field02");
 			if (helpField != null) {
 				System.out.println(helpField.getText());
