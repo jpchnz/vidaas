e5f25d920718a4f83edf731c5826618a13dbca5e
thestoat <thestoat@gmail.com>
2012-02-13 11:14:18 +0000

    Fixed possible npe with in-context help text

--- vidaas3/trunk/vidaas/src/hot/uk/ac/ox/oucs/vidaas/session/HelpController.java
+++ vidaas3/trunk/vidaas/src/hot/uk/ac/ox/oucs/vidaas/session/HelpController.java
@@ -1,14 +1,10 @@
 package uk.ac.ox.oucs.vidaas.session;
 
-import java.io.FileNotFoundException;
-
 import javax.xml.bind.JAXBException;
 
 import org.jboss.seam.ScopeType;
-import org.jboss.seam.annotations.Logger;
 import org.jboss.seam.annotations.Name;
 import org.jboss.seam.annotations.Scope;
-import org.jboss.seam.log.Log;
 
 import uk.ac.ox.oucs.vidaas.session.generated.HelpField;
 
@@ -17,25 +13,25 @@ import uk.ac.ox.oucs.vidaas.session.generated.HelpField;
 public class HelpController {
 	private HelpTextGenerator helpTextGenerator;
 	private String helpId = null;
-	@Logger
-	private Log log;
 	
 	public HelpController() {
-		if (true) {
-			helpTextGenerator = null;
-			return;
-		}
+//		if (true) {
+//			helpTextGenerator = null;
+//			return;
+//		}
 		try {
-			helpTextGenerator = new HelpTextGenerator(log);
-		}
-		catch (FileNotFoundException e) {
-			e.printStackTrace();
+			helpTextGenerator = new HelpTextGenerator();
 		}
 		catch (JAXBException e) {
 			e.printStackTrace();
 		}
 	}
 	
+	
+	/**
+	 * @param helpId the id of the required help
+	 * @return the associated helptext
+	 */
 	public String setupHelpText(String helpId) {
 		if (helpTextGenerator == null) {
 			return "No help available";
@@ -48,6 +44,9 @@ public class HelpController {
 		return helpField.getText();
 	}
 
+	/**
+	 * @return helptext for previously setup id
+	 */
 	public String getHelpText() {
 		if ( (helpId == null) || (helpTextGenerator == null) ) {
 			return "No help available";
@@ -59,6 +58,10 @@ public class HelpController {
 		return helpField.getText();
 	}
 	
+	/**
+	 * @param helpId the id of the help required
+	 * @return the title of the help for the specified id
+	 */
 	public String getHelpTitle(String helpId) {
 		if (helpTextGenerator == null) {
 			return "No help available";
@@ -71,6 +74,11 @@ public class HelpController {
 		return helpField.getTitle();
 	}
 
+	
+	/**
+	 * Set the help id for future use
+	 * @param helpId
+	 */
 	public void setHelpId(String helpId) {
 		this.helpId = helpId;
 	}
