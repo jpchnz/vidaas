b52dd95464f84ed2a2a7ad942196246435c02e23
aassif.akram@gmail.com <aassif.akram@gmail.com@24beba35-015c-934d-eeb6-620b1e2b2385>
2012-02-27 13:35:59 +0000

    Re-committing ... updated the old version by mistake

--- vidaas3/trunk/vidaas/src/hot/uk/ac/ox/oucs/vidaas/session/HelpController.java
+++ vidaas3/trunk/vidaas/src/hot/uk/ac/ox/oucs/vidaas/session/HelpController.java
@@ -1,7 +1,5 @@
 package uk.ac.ox.oucs.vidaas.session;
 
-import java.io.FileNotFoundException;
-
 import javax.xml.bind.JAXBException;
 
 import org.jboss.seam.ScopeType;
@@ -13,57 +11,75 @@ import uk.ac.ox.oucs.vidaas.session.generated.HelpField;
 @Name("helpController")
 @Scope(ScopeType.SESSION)
 public class HelpController {
-	private HelpTextGenerator helpTextGenerator;
-	private String helpId = null;
-	
-	public HelpController() {
-		try {
-			helpTextGenerator = new HelpTextGenerator();
-		}
-		catch (FileNotFoundException e) {
-			e.printStackTrace();
-		}
-		catch (JAXBException e) {
-			e.printStackTrace();
-		}
-	}
-	
-	public String setupHelpText(String helpId) {
-		if (helpTextGenerator == null) {
-			return "No help available";
-		}
-		HelpField helpField = helpTextGenerator.getHelpField(helpId);
-		if (helpField == null) {
-			return "No help available";
-		}
-		this.helpId = helpId;
-		return helpField.getText();
-	}
+    private HelpTextGenerator helpTextGenerator;
+    private String helpId = null;
+    
+    public HelpController() {
+//        if (true) {
+//            helpTextGenerator = null;
+//            return;
+//        }
+        try {
+            helpTextGenerator = new HelpTextGenerator();
+        }
+        catch (JAXBException e) {
+            e.printStackTrace();
+        }
+    }
+    
+    
+    /**
+     * @param helpId the id of the required help
+     * @return the associated helptext
+     */
+    public String setupHelpText(String helpId) {
+        if (helpTextGenerator == null) {
+            return "No help available";
+        }
+        HelpField helpField = helpTextGenerator.getHelpField(helpId);
+        if (helpField == null) {
+            return "No help available";
+        }
+        this.helpId = helpId;
+        return helpField.getText();
+    }
 
-	public String getHelpText() {
-		if ( (helpId == null) || (helpTextGenerator == null) ) {
-			return "No help available";
-		}
-		HelpField helpField = helpTextGenerator.getHelpField(helpId);
-		if (helpField == null) {
-			return "No help available";
-		}
-		return helpField.getText();
-	}
-	
-	public String getHelpTitle(String helpId) {
-		if (helpTextGenerator == null) {
-			return "No help available";
-		}
-		HelpField helpField = helpTextGenerator.getHelpField(helpId);
-		if (helpField == null) {
-			return "No help available";
-		}
-		this.helpId = helpId;
-		return helpField.getTitle();
-	}
+    /**
+     * @return helptext for previously setup id
+     */
+    public String getHelpText() {
+        if ( (helpId == null) || (helpTextGenerator == null) ) {
+            return "No help available";
+        }
+        HelpField helpField = helpTextGenerator.getHelpField(helpId);
+        if (helpField == null) {
+            return "No help available";
+        }
+        return helpField.getText();
+    }
+    
+    /**
+     * @param helpId the id of the help required
+     * @return the title of the help for the specified id
+     */
+    public String getHelpTitle(String helpId) {
+        if (helpTextGenerator == null) {
+            return "No help available";
+        }
+        HelpField helpField = helpTextGenerator.getHelpField(helpId);
+        if (helpField == null) {
+            return "No help available";
+        }
+        this.helpId = helpId;
+        return helpField.getTitle();
+    }
 
-	public void setHelpId(String helpId) {
-		this.helpId = helpId;
-	}
+    
+    /**
+     * Set the help id for future use
+     * @param helpId
+     */
+    public void setHelpId(String helpId) {
+        this.helpId = helpId;
+    }
 }
