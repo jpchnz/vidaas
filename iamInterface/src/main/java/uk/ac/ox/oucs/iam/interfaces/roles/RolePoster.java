e98e64a0fa6605ff74a1e53fb356a5134ac68e5d
thestoat <thestoat@gmail.com>
2012-03-06 11:43:41 +0000

    Added timing so that if the auth server goes down, the system will only retry to contact it after 10 mins

--- iamInterface/src/main/java/uk/ac/ox/oucs/iam/interfaces/roles/RolePoster.java
+++ iamInterface/src/main/java/uk/ac/ox/oucs/iam/interfaces/roles/RolePoster.java
@@ -4,16 +4,16 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.net.NoRouteToHostException;
 import java.net.URL;
 import java.net.URLConnection;
+import java.util.Date;
 
 public class RolePoster {
 	private OutputStreamWriter out;
 	private String postData;
 	private URLConnection connection = null;
 	private URL url;
-	private boolean remoteServerUp;
+	private Date lastTry = null;
 	
 	
 	public RolePoster(URL url) {
@@ -21,35 +21,58 @@ public class RolePoster {
 	}
 	
 	
-	protected String sendPost(String postData) throws IOException {
+	protected String sendPost(String postData) {
+		if (lastTry != null) {
+			// We cannot currently access the remote server. 
+			Date now = new Date();
+			if ((now.getTime()-lastTry.getTime()) > (10*60*1000) ) {
+				/*
+				 * It has been 10 mins since we last tried to contact the server - try again
+				 */
+				lastTry = null;
+			}
+			else {
+				// Just say yes for now
+				return "true";
+			}
+		}
+		
 		this.postData = postData;
-		sendPost();
-		if (remoteServerUp) {
+		try {
+			System.out.println("Able to try to post to remote server");
+			sendPost();
 			return getResult();
 		}
-		else {
+		catch (Exception e) {
+			System.out.println("Auth server not contactable!");
+			lastTry = new Date();
 			return "true";
 		}
+		
+//		if (remoteServerUp) {
+//			sendPost();
+//		}
+//		
+//		if (remoteServerUp) {
+//			return getResult();
+//		}
+//		else {
+//			System.out.println("Auth server not up!");
+//			return "true";
+//		}
 	}
 	
 	/**
 	 * Post the data set in variable:postData to the web service defined in the constructor
 	 * @throws IOException
 	 */
-	private void sendPost() {
-		try {
-			connection = url.openConnection();
-			connection.setDoOutput(true);
-			out = new OutputStreamWriter(connection.getOutputStream());
-			out.write(postData);
-			out.flush();
-			out.close();
-			remoteServerUp = true;
-		}
-		catch (Exception e) {
-			remoteServerUp = false;
-			System.out.println("Cannot find the remote server to post commands to.");
-		}
+	private void sendPost() throws Exception {
+		connection = url.openConnection();
+		connection.setDoOutput(true);
+		out = new OutputStreamWriter(connection.getOutputStream());
+		out.write(postData);
+		out.flush();
+		out.close();
 	}
 	
 	
@@ -58,7 +81,7 @@ public class RolePoster {
 	 * @return the output from the web servlet as a String
 	 * @throws IOException
 	 */
-	private String getResult() throws IOException {
+	private String getResult() throws Exception {
 		BufferedReader in = null;
 		String result = "";
 
