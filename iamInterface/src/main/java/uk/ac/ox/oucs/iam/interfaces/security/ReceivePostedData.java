b346993405c085669631693b639d2bae07a60fc0
thestoat@gmail.com <thestoat@gmail.com@24beba35-015c-934d-eeb6-620b1e2b2385>
2012-03-30 10:13:11 +0000

    Added standardised support for http POST

--- iamInterface/src/main/java/uk/ac/ox/oucs/iam/interfaces/security/ReceivePostedData.java
+++ iamInterface/src/main/java/uk/ac/ox/oucs/iam/interfaces/security/ReceivePostedData.java
@@ -9,105 +9,129 @@ import java.net.URLConnection;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
 import org.apache.log4j.Logger;
 
+import uk.ac.ox.oucs.iam.interfaces.utilities.GeneralUtils;
 import uk.ac.ox.oucs.iam.interfaces.utilities.SystemVars;
-
 
 public class ReceivePostedData {
 	private static Logger log = Logger.getLogger(ReceivePostedData.class);
-	
+
 	public static final String REQUEST_DATA_CODE = "requestCurrent=data";
 	public static final String REQUEST_DATA_CODE_DONT_CLEAR_STACK = "requestCurrent=dataNoClear";
-	
-	
-	
+
 	/**
-	 * Receive pending messages from the IAM secure message receiver, clearing the stack once processed.
+	 * Receive pending messages from the IAM secure message receiver, clearing
+	 * the stack once processed.
 	 * 
-	 * @return a list of SecurePostData objects defining the messages destined for the service
+	 * @return a list of SecurePostData objects defining the messages destined
+	 *         for the service
 	 * @throws IOException
 	 */
 	public static List<SecurePostData> getPendingMessageDataAndClear() throws IOException {
 		log.debug("getPendingMessageDataAndClear");
 		return getPendingMessageData(true);
 	}
-	
+
 	/**
-	 * Receive pending messages from the IAM secure message receiver but do not clear the stack once processed.
+	 * Receive pending messages from the IAM secure message receiver but do not
+	 * clear the stack once processed.
 	 * 
-	 * @return a list of SecurePostData objects defining the messages destined for the service
+	 * @return a list of SecurePostData objects defining the messages destined
+	 *         for the service
 	 * @throws IOException
 	 */
 	public static List<SecurePostData> getPendingMessageDataAndKeep() throws IOException {
 		log.debug("getPendingMessageDataAndKeep");
 		return getPendingMessageData(false);
 	}
-	
+
 	private static List<SecurePostData> getPendingMessageData(boolean clear) throws IOException {
+		String result = "";
+
 		if (log.isDebugEnabled()) {
 			log.debug(String.format("About to post command to %s", SystemVars.ADDRESS_OF_IAM_WEBAPP_RECEIVER));
 		}
-		
-		URL url = new URL(SystemVars.ADDRESS_OF_IAM_WEBAPP_RECEIVER);
-		URLConnection connection = url.openConnection();
-		connection.setDoOutput(true);
-		OutputStreamWriter out = new OutputStreamWriter(connection.getOutputStream());
-		
-		if (clear) {
-			out.write(REQUEST_DATA_CODE);
+		boolean test = true;
+
+		if (test) {
+			List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>(1);
+			nameValuePairs.add(new BasicNameValuePair("requestCurrent",
+					"data"));
+			result = GeneralUtils.sendStandardHttpPost(SystemVars.ADDRESS_OF_IAM_WEBAPP_RECEIVER, nameValuePairs);
+			if (log.isDebugEnabled()) {
+				log.debug("Got result: " + result);
+			}
 		}
 		else {
-			out.write(REQUEST_DATA_CODE_DONT_CLEAR_STACK);
-		}
+			URL url = new URL(SystemVars.ADDRESS_OF_IAM_WEBAPP_RECEIVER);
+			URLConnection connection = url.openConnection();
+			connection.setDoOutput(true);
+			OutputStreamWriter out = new OutputStreamWriter(connection.getOutputStream());
 
-		out.flush();
-		out.close();
-		
-		BufferedReader in = null;
-		String result = "";
-
-		in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-
-		String decodedString;
-
-		boolean firstLine = true;
-		while ((decodedString = in.readLine()) != null) {
-			if (firstLine) {
-				firstLine = false;
-				continue;
+			if (clear) {
+				out.write(REQUEST_DATA_CODE);
 			}
-			result += decodedString + "\n";
+			else {
+				out.write(REQUEST_DATA_CODE_DONT_CLEAR_STACK);
+			}
+
+			out.flush();
+			out.close();
+
+			BufferedReader in = null;
+
+			in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+
+			String decodedString;
+
+			boolean firstLine = true;
+			while ((decodedString = in.readLine()) != null) {
+				if (firstLine) {
+					firstLine = false;
+					continue;
+				}
+				result += decodedString + "\n";
+			}
+			in.close();
 		}
-		in.close();
-		
+
 		List<SecurePostData> securePostDataList = new ArrayList<SecurePostData>();
 		if (result.length() != 0) {
 			securePostDataList = SecurePostData.getObjectFromString(result);
 		}
-		
+
 		return securePostDataList;
 	}
-	
+
 	public static void main(String[] args) {
 		try {
 			List<SecurePostData> securePostDataList = ReceivePostedData.getPendingMessageDataAndClear();
-			if ( (securePostDataList == null) || (securePostDataList.size() == 0) ) {
+			if ((securePostDataList == null) || (securePostDataList.size() == 0)) {
 				System.out.println("No data returned");
 			}
 			else {
-				System.out.println(String.format("We have %s piece%s of data", securePostDataList.size(), securePostDataList.size() > 1 ? "s" : ""));
+				System.out.println(String.format("We have %s piece%s of data", securePostDataList.size(),
+						securePostDataList.size() > 1 ? "s" : ""));
 				int counter = 0;
 				for (SecurePostData spd : securePostDataList) {
-					System.out.println("Item " + (counter+1));
-					System.out.println("Originator for data " + (counter+1) + " = " + spd.getOriginatorHost());
+					System.out.println("Item " + (counter + 1));
+					System.out.println("Originator for data " + (counter + 1) + " = " + spd.getOriginatorHost());
 					for (String s : spd.getPostParms().values()) {
 						System.out.println(s);
 					}
 					counter++;
 				}
 			}
-		} catch (IOException e) {
+		}
+		catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
