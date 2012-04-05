abff8f0897240c3e585c4b5cc031977f5ea31634
thestoat <thestoat@24beba35-015c-934d-eeb6-620b1e2b2385>
2012-04-05 15:59:46 +0000

    Added documentation

--- iamInterface/src/main/java/uk/ac/ox/oucs/iam/interfaces/utilities/SystemVars.java
+++ iamInterface/src/main/java/uk/ac/ox/oucs/iam/interfaces/utilities/SystemVars.java
@@ -8,23 +8,37 @@ package uk.ac.ox.oucs.iam.interfaces.utilities;
  *
  */
 public class SystemVars {
+	/**
+	 * The full address of the IAM role serlvet
+	 */
 	//public static final String ADDRESS_OF_IAM_WEBAPP = "http://129.67.241.38/iam/ProjectRoleServlet";
 	public static final String ADDRESS_OF_IAM_WEBAPP = "http://129.67.241.38/iam/ProjectRoleServlet";
 	
+	
+	/**
+	 * The address of the servlet that can receive secure POST data, validate it and 
+	 * pass on to the destination
+	 */
 	public static final String ADDRESS_OF_IAM_WEBAPP_RECEIVER = "http://localhost:8080/iam/ReceivePost";;//"http://129.67.241.38/iam/ReceivePost";
 	
-	public static final String ADDRESS_OF_VIDAAS_BILLING_APP = "http://localhost:8080/vidaasBilling";//"http://129.67.103.124:8081/vidaasBilling";
+	/**
+	 * The address of the billing webapp
+	 */
+	public static final String ADDRESS_OF_VIDAAS_BILLING_APP = "http://localhost:8080/vidaasBilling/BillingServlet";//"http://129.67.103.124:8081/vidaasBilling";
 	
+	
+	/*
+	 * Here follows some standard constants used for posting and receiving data, so that, for example, we should never
+	 * have the problem where a sender might send a parameter key of "projectuser" and the receiver expect "projectUser"
+	 */
 	public static final String POST_COMMAND_PROVIDE_UUID_OF_PUBLIC_KEY = "provideUuid";
 	public static final String POST_COMMAND_PROVIDE_PUBLIC_KEY = "providePublicKey";
-	
 	public static final String POST_COMMAND_EMAIL_TOKEN = "u";
 	public static final String POST_COMMAND_COMMAND_TOKEN = "c";
 	public static final String POST_COMMAND_PROJECTNAME_TOKEN = "projectName";
 	public static final String POST_COMMAND_PROJECTSPACE_TOKEN = "space";
 	public static final String POST_COMMAND_BILLINGFREQUENCY_TOKEN = "billingFrequency";
 	public static final String POST_COMMAND_PROJECTID_TOKEN = "projectId";
-	
 	public static final String POST_COMMAND_NEW_BILL = "bill";
 	public static final String POST_COMMAND_NEW_PROJECT = "newProject";
 	public static final String POST_COMMAND_UPDATE_PROJECT = "updateProject";
@@ -32,6 +46,12 @@ public class SystemVars {
 	public static final String POST_COMMAND_NEW_DATA_AVAILABLE = "comeAndGetit";
 	
 	
+	/**
+	 * Final location of the keystore. This is the folder where keys, as files, are placed.
+	 * TODO
+	 * There is a work in progress to have public keys placed in a proper keystore and
+	 * public keys added to a local database.
+	 */
 	public static final String locationOfKeyStore = "/User/dave/keyStore/";
 	
 	
